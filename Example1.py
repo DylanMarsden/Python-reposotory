eea222d8e5d530541974bccb72093b9918110d1a
DylanMarsden <dylanpmarsden@icloud.com>
2025-01-12 15:34:33 +0000

    first readme

--- Example1.py
+++ Example1.py
@@ -1,3 +1,24 @@
 print("Hello my name is Dylan")
 name=input("What is your name? ")
 print("Nice to meet you",name )
+
+
+myage = None
+
+while myage is None:
+    inputmyage=input("How old are you? ")
+
+    try:
+        myage=int(inputmyage)
+        if type(myage) == int:
+            print("Wow",myage," is a nice age")
+        else:
+            print("sorry that isnt a number")
+        if myage==100:
+            print("congratulations on making it to 100")
+        gender=input("Are you a girl a boy or non binary?")
+        print("Aren't you a big",gender)
+
+    except ValueError:
+        print("Dodgy entry")
+
